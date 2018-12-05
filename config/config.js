a3d8611e899746afdf0493b913d9934a2ebb076d
abenizri <avibenb@gmail.com>
2018-12-05 11:01:50 +0200

    updated mongo ip

--- config/config.js
+++ config/config.js
@@ -13,7 +13,7 @@ global.log = bunyan.createLogger({
 var config = {
   "production": {
     //"database": "mongodb://<user>:<pwd>@apollo.modulusmongo.net:27017/db",
-    "database": "mongodb://18.223.162.72:27017/let_us_know"
+    "database": "mongodb://18.191.31.190:27017/let_us_know"
   },
   "default": {
     "database": "mongodb://127.0.0.1:27017/let_us_know"
